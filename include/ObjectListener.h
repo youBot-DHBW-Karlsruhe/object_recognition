ca80267a9582ebd5b981e795a397814072930aad
CodeLionX <sebastian.schmidl@t-online.de>
2017-03-05 21:21:21 +0100

    publishing own message type now

--- include/ObjectListener.h
+++ include/ObjectListener.h
@@ -5,6 +5,7 @@
 #include <find_object_2d/ObjectsStamped.h>
 #include <tf/transform_listener.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <object_recognition/ObjectPosition.h>
 
 namespace cleaner {
 
@@ -17,16 +18,15 @@ private:
     tf::TransformListener tfListener_;
     ros::Subscriber subs_;
     ros::Publisher pub_;
-    std_msgs::Float32MultiArray coordinates;
+    object_recognition::ObjectPosition position_;
 
 public:
 
     ObjectListener(ros::NodeHandle nh) {
         mapFrameId_ = "/map";
         objFramePrefix_ = "object";
-        coordinates = std_msgs::Float32MultiArray();
         subs_ = nh.subscribe("/objectsStamped", 1, &ObjectListener::objectsDetectedCallback, this);
-        pub_ = nh.advertise<std_msgs::Float32MultiArray>("object_position", 1);
+        pub_ = nh.advertise<object_recognition::ObjectPosition>("object_position", 1);
     }
 
     void objectsDetectedCallback(const find_object_2d::ObjectsStampedConstPtr & msg){
@@ -41,6 +41,7 @@ public:
                         std::stringstream ss;
                         ss << "object_" << id;
                         std::string objectFrameId = ss.str();
+                        position_.object_id = id;
                         ROS_INFO("object_%d", id);
                         tf::StampedTransform pose;
                         tf::StampedTransform poseCam;
@@ -58,18 +59,19 @@ public:
                             continue;
                         }
 
-                        coordinates.data.push_back(pose.getOrigin().x());
-                        coordinates.data.push_back(pose.getOrigin().y());
-                        coordinates.data.push_back(pose.getOrigin().z());
-                        coordinates.data.push_back(pose.getRotation().x());
-                        coordinates.data.push_back(pose.getRotation().y());
-                        coordinates.data.push_back(pose.getRotation().z());
-                        coordinates.data.push_back(pose.getRotation().w());
+                        object_recognition::ObjectPosition position;
+                        position.pose.position.x = pose.getOrigin().x();
+                        position.pose.position.y = pose.getOrigin().y();
+                        position.pose.position.z = pose.getOrigin().z();
+                        position.pose.orientation.x = pose.getRotation().x();
+                        position.pose.orientation.x = pose.getRotation().y();
+                        position.pose.orientation.x = pose.getRotation().z();
+                        position.pose.orientation.x = pose.getRotation().w();
 
                         //TODO aggregate received data to ensure quality and useful grab positions
 
                         //publish data
-                        publishPosition();
+                        publishPosition(position);
 
 
                     }
@@ -77,9 +79,8 @@ public:
 
     }
 
-    void publishPosition() {
-        pub_.publish(coordinates);
-        coordinates.data.clear();
+    void publishPosition(object_recognition::ObjectPosition position) {
+        pub_.publish(position);
     }
 
 };
